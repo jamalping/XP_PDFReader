d87ed9ed2966752b0bff5d127fdb597536c4e292
jamalping <420436097@qq.com>
2018-10-29 18:12:05 +0800

    0.0.3

--- upload.sh
+++ upload.sh
@@ -55,5 +55,6 @@ git add .
 git commit -am ${NewVersionNumber}
 git tag ${NewVersionNumber}
 git push origin master --tags
+pod trunk push XP_PDFReader.podspec --verbose --allow-warnings --use-libraries
 #pod repo update Specs && pod repo push Specs XP_PDFReader.podspec --verbose --allow-warnings --sources='git@git.silvrr.com:iOS/Specs.git,https://github.com/CocoaPods/Specs'
- cd ~/.cocoapods/repos/Specs && git pull origin master && cd - && pod repo push Specs XP_PDFReader.podspec --verbose --allow-warnings --use-libraries
+# cd ~/.cocoapods/repos/Specs && git pull origin master && cd - && pod repo push Specs XP_PDFReader.podspec --verbose --allow-warnings --use-libraries
